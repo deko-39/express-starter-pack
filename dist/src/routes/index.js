3bd7916ec7938566080dfec07633d1243709e01c
Deko39 <tam.nguyen.cdt@gmail.com>
2023-07-08 12:14:35 +0700

    fix

--- dist/src/routes/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tslib_1 = require("tslib");
-const express_1 = tslib_1.__importDefault(require("express"));
-const user_1 = tslib_1.__importDefault(require("./user"));
-const router = express_1.default.Router();
-router.use('/users', user_1.default);
-exports.default = router;
